01f9d54e0875fe548c9ef68499997781547bf87d
KaiqiZhang <alexzhangkaiqi@gmail.com>
2017-08-02 12:58:01 -0400

    Add groups app.

---
+++ simplesocial/groups/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+class GroupMemberInline(admin.TabularInline):
+    model = model.GroupMember
+    
+admin.site.register(models.Group)
